71517e5b927251655f79caec9c4bf603764eb59e
TaeHyeon Kim <kth990303@naver.com>
2021-09-21 14:24:19 +0900

    Update 4013_scc_topology.cpp
    // 210921 #4013 ATM Platinum II
    // SCC + topology_sort

--- 4013_scc_topology.cpp
+++ 4013_scc_topology.cpp
@@ -1,35 +1,36 @@
-// 200829 #4013 ATM Platinum II
-// SCC 후, 조건 고려하여 SCC끼리 위상정렬 다 해보는 문제
-// 호흡이 길고 꽤나 어려움
-// 2시간이 증발함
-// 다행히 로직은 처음부터 잘 짰으나, 구현이 잘못됨
-// 라이(kks227)님 블로그 참고함
+// 210921 #4013 ATM Platinum II
+// SCC + topology_sort
 #include <iostream>
 #include <algorithm>
-#include <stack>
-#include <queue>
+#include <string>
 #include <vector>
-const int MAX = 500001;
-const int DNF = 1000000007;
+#include <cstring>
+#include <queue>
+#include <cmath>
+#include <stack>
+#include <set>
+#include <map>
+#define all(v) (v).begin(), (v).end()
+#define press(v) (v).erase(unique(all(v)), (v).end())
 using namespace std;
-int N, M, id, d[MAX], w[MAX], sccNum[MAX];
-bool finished[MAX], isEat[MAX];
-vector<int> v[MAX];
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<int, pi> pii;
+const int MAX = 500001;
+const int INF = 0x3f3f3f3f;
+int N, M, d[MAX], id, sccNum[MAX], w[MAX], S, cnt;
+int in[MAX], sccW[MAX], sccMaxW[MAX];
+vector<int> v[MAX], c[MAX];
 vector<vector<int>> SCC;
+bool finished[MAX], restaurant[MAX], sccRes[MAX], can[MAX];
 stack<int> s;
-// 위상정렬 세팅
-int sccW[MAX], in[MAX];	// scc 돈 합, indegree
-vector<int> adj[MAX];	
-bool sccHasEat[MAX];
-bool can[MAX];
-int ans[MAX];
 int dfs(int x) {
-	d[x] = ++id;
+	d[x] = id++;
 	s.push(x);
-	
+
 	int parent = d[x];
 	for (auto i : v[x]) {
-		if (!d[i])
+		if (d[i] == -1)
 			parent = min(parent, dfs(i));
 		else if (!finished[i])
 			parent = min(parent, d[i]);
@@ -41,7 +42,7 @@ int dfs(int x) {
 			s.pop();
 			scc.push_back(t);
 			finished[t] = true;
-			sccNum[t] = SCC.size() + 1;
+			sccNum[t] = SCC.size();
 			if (t == x)
 				break;
 		}
@@ -50,78 +51,71 @@ int dfs(int x) {
 	return parent;
 }
 int main() {
-	cin.tie(0);
-	cout.tie(0);
-	ios::sync_with_stdio(false);
+	cin.tie(0)->sync_with_stdio(0);
 
 	cin >> N >> M;
-	while (M--) {
+	for (int i = 0; i < M; i++) {
 		int a, b;
 		cin >> a >> b;
+		a--; b--;
 		v[a].push_back(b);
 	}
-	// SCC 만들기
-	for (int i = 1; i <= N; i++) {
-		if (!d[i])
+	fill(d, d + N, -1);
+	// O(N)
+	for (int i = 0; i < N; i++) {
+		if (d[i] == -1)
 			dfs(i);
 	}
-	// 돈 정보 입력
-	for (int i = 1; i <= N; i++) {
+	for (int i = 0; i < N; i++) {
 		cin >> w[i];
 	}
-	int s, k;
-	cin >> s >> k;
-	// 레스토랑 여부 입력 (예제의 경우 전부 보유)
-	for (int i = 1; i <= k; i++) {
-		int num;
-		cin >> num;
-		isEat[num] = true;
+	cin >> S >> cnt;
+	S--;
+	S = sccNum[S];
+	for (int i = 0; i < cnt; i++) {
+		int n;
+		cin >> n;
+		restaurant[--n] = true;
 	}
-	// 정보 입력 끝.
-	for (int i = 1; i <= N; i++) {
+	// scc ordering
+	for (int i = 0; i < N; i++) {
 		sccW[sccNum[i]] += w[i];
-		// scc가 출발 지점을 가지고 있는가?
-		if (s == i)
-			s = sccNum[i];
-		// scc가 레스토랑을 가지고 있는가?
-		if (isEat[i])
-			sccHasEat[sccNum[i]] = true;
-
-		for (int next : v[i]) {
-			if (sccNum[next] == sccNum[i])
-				continue;
-			// scc가 다른 경우 인접 scc 넣어줌
-			adj[sccNum[i]].push_back(sccNum[next]);
-			// 위상정렬을 위한 행위
-			in[sccNum[next]]++;
+		if (restaurant[i])
+			sccRes[sccNum[i]] = true;
+		for (auto j : v[i]) {
+			if (sccNum[j] != sccNum[i]) {
+				c[sccNum[i]].push_back(sccNum[j]);
+				in[sccNum[j]]++;
+			}
 		}
 	}
-	int res = 0;
 	queue<int> q;
-	for (int i = 1; i <= N; i++) {
-		w[i] = sccW[i];	// scc 액수만큼 초기화
-		if (i == s)
-			can[i] = true;
+	for (int i = 0; i < SCC.size(); i++) {
+		sccMaxW[i] = sccW[i];
 		if (!in[i])
 			q.push(i);
 	}
-	// SCC 끼리 위상정렬
+	can[S] = true;
+	int ans = 0;
+	// O(N+M)
+	// if many early queue elements, can exist that we can't go points
+	// so make can[MAX] array
 	while (!q.empty()) {
-		int x = q.front();
+		int cur = q.front();
 		q.pop();
-		for (auto i : adj[x]) {
-			if (can[x]) {
-				w[i] = max(w[i], w[x] + sccW[i]);
+		for (auto i : c[cur]) {
+			if (can[cur]) {
+				sccMaxW[i] = max(sccMaxW[i], sccMaxW[cur] + sccW[i]);
 				can[i] = true;
 			}
-			in[i]--;
-			if (!in[i])
+			if (--in[i]==0) {
 				q.push(i);
+			}
 		}
 	}
-	for (int i = 1; i <= SCC.size(); i++) {
-		if (sccHasEat[i] && can[i])
-			res = max(res, w[i]);
+	for (int i = 0; i < SCC.size(); i++) {
+		if (can[i] && sccRes[i])
+			ans = max(ans, sccMaxW[i]);
 	}
-	cout << res << "\n";
+	cout << ans << "\n";
 }
