5e35b329b6f42b5f21b79232d2aaf4cc890e5b57
kth990303 <kth990303@naver.com>
2022-08-21 12:01:43 +0900

    build: update template 220821
    편리성을 위한 환경마다 다른 입출력 템플릿 구축

---
+++ templates/normal_template_220821.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define sz(v) (int)v.size()
+#define int long long
+#define all(v) (v).begin(), (v).end()
+#define press(v) (v).erase(unique(all(v)), (v).end())
+using namespace std;
+typedef pair<int, int> pi;
+typedef pair<int,pi> pii;
+const int MAX = 2e5+7;
+const int INF = 0x3f3f3f3f3f3f3f3f;
+const int MOD = 1e9 + 7;
+int t;
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+    int N,M,a[MAX],ans=0;
+    cin>>N;
+
+}
---
+++ templates/scpc_template_220821.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+#define sz(v) (int)v.size()
+#define int long long
+#define all(v) (v).begin(), (v).end()
+#define press(v) (v).erase(unique(all(v)), (v).end())
+using namespace std;
+typedef pair<int, int> pi;
+typedef pair<int,pi> pii;
+const int MAX = 2e5+7;
+const int INF = 0x3f3f3f3f3f3f3f3f;
+const int MOD = 1e9 + 7;
+int T;
+int32_t main() {
+    cin.tie(0)->sync_with_stdio(0);
+    cin>>T;
+    for (int t = 1; t <= T; t++) {
+        int N,M,a[MAX],ans=0;
+        cin>>N;
+
+        cout<<"Case #"<<t<<"\n";
+        cout<<ans<<"\n";
+    }
+}
