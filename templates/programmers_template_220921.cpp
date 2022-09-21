0d0aca23964fa9b449bc055794dee3d3fd81a8e1
kth990303 <kth990303@naver.com>
2022-09-21 13:47:47 +0900

    build: create template to programmers
    cannot use int32_t and define int long long

---
+++ templates/programmers_template_220921.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+#define sz(v) (int)v.size()
+#define all(v) (v).begin(), (v).end()
+#define press(v) (v).erase(unique(all(v)), (v).end())
+using namespace std;
+typedef long long ll;
+typedef pair<int, int> pi;
+typedef pair<int,pi> pii;
+typedef pair<ll, ll> pl;
+typedef pair<ll, pl> pll;
+const int MAX = 5e4+7;
+const int INF = 0x3f3f3f3f3f3f3f3f;
+const ll LNF=0x3f3f3f3f3f3f3f3f;
+const int MOD = 1e9 + 7;
+int N;
+int main() {
+    cin.tie(0)->sync_with_stdio(0);
+    N=7;
+
+
+    vector<int> ans;
+    for(auto i:ans){
+        cout<<i<<" ";
+    }
+}
